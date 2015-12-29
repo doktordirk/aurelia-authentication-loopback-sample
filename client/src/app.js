4ed27441204deb311e727a97667af8e2d392e9b1
doktordirk <doktordirk@outlook.de>
2015-12-29 22:06:42 +0100

    refactor(client-childrouter): removed

--- client/src/app.js
+++ client/src/app.js
@@ -4,7 +4,6 @@ export class App {
     config.map([
       { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
       { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' },
       { route: 'customer',      name: 'customer',     moduleId: 'modules/customer/index', nav: true, title:'Crm' }
     ]);
 
