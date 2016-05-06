91e3c5c5776677754f814b2bb446d547239f188c
doktordirk <doktordirk@outlook.de>
2016-05-06 12:43:55 +0200

    fix(clients): remove post logout

--- client/src/modules/auth/logout.js
+++ client/src/modules/auth/logout.js
@@ -5,27 +5,10 @@ import {AuthService} from 'aurelia-authentication';
 export class Logout {
   constructor(auth) {
     this.auth = auth;
-    this.client = auth.client;
   }
 
   activate() {
-    /* for JWT:
-    *  since its stateless, we only need to delete the token in the browsers storage
-
+    // since its stateless, we only need to delete the token in the browsers storage
     this.auth.logout();
-
-    */
-
-    // for default loopback:
-    // since it uses sessionToken, we need to logout on the server
-    this.client.post('logout')
-    .then(() => {
-      // delete local token and redirect in any case
-      this.auth.logout();
-    })
-    .catch(() => {
-      // same on error. browser storage must be clean
-      this.auth.logout();
-    })
   }
 }
