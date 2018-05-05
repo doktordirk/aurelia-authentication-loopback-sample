f98c2e3ee50864f6b39759e4e8b254a27141051e
doktordirk <doktordirk@outlook.de>
2018-05-05 19:41:03 +0200

    fix(webpack): add moduleName to child routes

--- skeleton-esnext-webpack/src/modules/customer/index.js
+++ skeleton-esnext-webpack/src/modules/customer/index.js
@@ -1,9 +1,11 @@
+import {PLATFORM} from 'aurelia-pal';
+
 export class Index {
   configureRouter(config, router) {
     config.map([
-      {route: ['', 'list'], moduleId: './list', name: 'list'},
-      {route: 'edit/:id', moduleId: './edit', name: 'edit'},
-      {route: 'create', moduleId: './edit', name: 'create'}
+      {route: ['', 'list'],  moduleId: PLATFORM.moduleName('./list'), name: 'list'},
+      {route: 'edit/:id',  moduleId: PLATFORM.moduleName('./edit'), name: 'edit'},
+      {route: 'create',  moduleId: PLATFORM.moduleName('./edit'), name: 'create'}
     ]);
 
     this.router = router;
